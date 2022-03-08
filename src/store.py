7d0364afcbd3ec67f53e318355a3e4fb4f82f3f4
Surya Gunasekaran <z5248110@ad.unsw.edu.au>
2022-03-08 23:17:04 +1100

    db con now uses url, protects usr id and pwd, but have to find better way to hide url, store works

--- src/store.py
+++ src/store.py
@@ -1,41 +1,43 @@
 import xml.etree.ElementTree as ET
 import psycopg2
+import os
 
 def storeInvoice(xml: str, filename: str):
     assert isinstance(xml, str), 'Please provide the xml as a string'
 
 #Convert string to XML
     root = ET.fromstring(xml)
+    
 
 #Extract key from XML
-    list1 = []
-    for child in root.iter():
-        if child.text.strip():
-            list1.append(child.text)
+#     list1 = []
+#     for child in root.iter():
+#         if child.text.strip():
+#             list1.append(child.text)
 
-#Store key and XML in DB
+# #Store key and XML in DB
 
-    #Connect to DB
+#     #Connect to DB
     try:
-        conn = psycopg2.connect(dbname='drj7scqvv00fb',user='hugfbhqshfeuxo',password='bb21e74bd662eb54bbfb67841e33cb3994fee2526208ee3667c736777acd8658',host='ec2-44-195-191-252.compute-1.amazonaws.com',port='5432')
+      DATABASE_URL = "postgres://hugfbhqshfeuxo:bb21e74bd662eb54bbfb67841e33cb3994fee2526208ee3667c736777acd8658@ec2-44-195-191-252.compute-1.amazonaws.com:5432/drj7scqvv00fb"
+      conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
     #Open a cursor for db operations
-        cur = conn.cursor()
-        print("hi")
+      cur = conn.cursor()
     
     #Insert File Name and XML into 
-    #     sql = "INSERT INTO invoices VALUES (%s,XMLPARSE (DOCUMENT %s))"
-    #     val = (filename, xml)
-    #     cur.execute(sql,val)
+      sql = "INSERT INTO invoices VALUES (%s,XMLPARSE (DOCUMENT %s))"
+      val = (filename, xml)
+      cur.execute(sql,val)
 
-    # #Save changes
-    #     conn.commit()
+    #Save changes
+      conn.commit()
 
     #Close DB connection
-        cur.close()
-        conn.close()
-    except Exception:
-        print("Unable to connect to the database")
+      cur.close()
+      conn.close()
+    except Exception as e:
+      print(e)
 
 if __name__ == '__main__':
     storeInvoice("""<?xml version="1.0" encoding="UTF-8" standalone="no"?>
