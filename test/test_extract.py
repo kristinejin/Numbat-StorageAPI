7b5046f8a603281b2549d57e900ecdd68b6137ed
Matthew Druckman <matthew.druckman@gmail.com>
2022-03-10 23:36:04 +1100

    fixed tests with established extract return type

--- test/test_extract.py
+++ test/test_extract.py
@@ -9,7 +9,8 @@ from test.xmlString import xml_as_string
 
 DATABASE_URL = "postgres://hugfbhqshfeuxo:bb21e74bd662eb54bbfb67841e33cb3994fee2526208ee3667c736777acd8658@ec2-44-195-191-252.compute-1.amazonaws.com:5432/drj7scqvv00fb"
 
-def test_store():
+# test simple extraction
+def test_store_and_extract():
     fileName = (''.join(random.choice(string.ascii_lowercase) for i in range(4)) )
     storeInvoice(xml_as_string,fileName)
     conn = psycopg2.connect(DATABASE_URL, sslmode='require')
@@ -17,17 +18,18 @@ def test_store():
     cur = conn.cursor()
     
     
-    # assert extract(fileName)[0] = fileName
-    # assert extract(fileName)[1] = xml_as_string
-
     #Insert File Name and XML into 
-    filenamereturned, xmlreturned = extract(fileName)
-    assert filenamereturned == fileName
-    # sql = "SELECT filename FROM invoices WHERE FileName = %s"
-    # val = (fileName)
-    # cur.execute(sql,[val])
-    # retFileName = cur.fetchone()
-    # print(type(retFileName))
-    # retFileName = retFileName[0]
-    # assert retFileName == fileName
-    # cur.close()
+    returnedvalues = extract(fileName)
+    assert returnedvalues[0] == fileName
+    #assert returnedvalues[1] == xml_as_string
+
+# extract non-existant file
+def test_extract_file_not_found():
+    with pytest.raises(Exception):
+        extract(fileName + 'Not Correct')
+
+"""
+need additional testing for authorisation
+    - attempting to extract an invoice that user does not have access to
+"""
+
