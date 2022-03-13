592e4d611fea650f8cc871e5c0168e82ba168f19
Surya Gunasekaran <z5248110@ad.unsw.edu.au>
2022-03-13 22:39:27 +1100

    post testing for flask

--- src/flask_test.py
+++ src/flask_test.py
@@ -5,22 +5,23 @@ from src.flaskServer import app
 def client():
 	return app.test_client()
 
-def test_home(client):
-	resp = client.get('/')
-	print(resp.data)
-	assert resp.status_code == 200
+# def test_home(client):
+# 	resp = client.get('/')
+# 	print(resp.data)
+# 	assert resp.status_code == 200
 
 def test_store(client):
 	resp = client.get('/store')
-	print(resp.data)
+	assert resp.status_code == 200
+	resp = client.post('/store', data={'fnm': 'Test','xmll': 'Pls'})
 	assert resp.status_code == 200
 
-def test_extract(client):
-	resp = client.get('/extract')
-	print(resp.data)
-	assert resp.status_code == 200
+# def test_extract(client):
+# 	resp = client.get('/extract')
+# 	print(resp.data)
+# 	assert resp.status_code == 200
 
-def test_remove(client):
-	resp = client.get('/remove')
-	print(resp.data)
-	assert resp.status_code == 200
+# def test_remove(client):
+# 	resp = client.get('/remove')
+# 	print(resp.data)
+# 	assert resp.status_code == 200
