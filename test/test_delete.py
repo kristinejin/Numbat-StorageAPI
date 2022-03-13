42d8508e0e4ae57bb204f4c3a53b515da8760aae
Peter Mo <pmo@Peters-MacBook-Air.local>
2022-03-13 12:40:52 +1100

    added test for non existant file

--- test/test_delete.py
+++ test/test_delete.py
@@ -12,37 +12,37 @@ import random
 import psycopg2
 from src.store import storeInvoice
 from src.remove import removeInvoice
+from src.extract import extract
 from test.xmlString import xml_as_string
 #import xml.etree.ElementTree as ET
 
-DATABASE_URL = "postgres://hugfbhqshfeuxo:bb21e74bd662eb54bbfb67841e33cb3994fee2526208ee3667c736777acd8658@ec2-44-195-191-252.compute-1.amazonaws.com:5432/drj7scqvv00fb"
+#DATABASE_URL = "postgres://hugfbhqshfeuxo:bb21e74bd662eb54bbfb67841e33cb3994fee2526208ee3667c736777acd8658@ec2-44-195-191-252.compute-1.amazonaws.com:5432/drj7scqvv00fb"
 
 def test_delete():
-    fileName = (''.join(random.choice(string.ascii_lowercase) for i in range(4)) )
+    # store a file 
+    fileName = (''.join(random.choice(string.ascii_lowercase) for i in range(4)))
     storeInvoice(xml_as_string,fileName)
 
-    conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-    #Open a cursor for db operations
-    cur = conn.cursor()
-    
-    #Insert File Name and XML into 
-    sql = " SELECT fileName FROM invoices WHERE fileName = %s"
-    val = (fileName)
-    cur.execute(sql,[val])
-    retFileName = cur.fetchone()
-    retFileName = retFileName[0]
-    assert retFileName == fileName
+    # double check file has been stored
+    output = extract(fileName)
+    assert output[0] == fileName
 
-    #confirmed file saved
-    #now delete
+    # delete the file
     removeInvoice(fileName)
-    sql = " SELECT fileName FROM invoices WHERE fileName = %s"
-    val = (fileName)
-    cur.execute(sql,[val])
-    # retFileName = cur.fetchone()
-    # retFileName = retFileName[0]
-    # assert retFileName == None
-    assert cur.fetchone() == None
+    output = extract(fileName)
+    assert output == None
 
-    cur.close()
-    
+def test_file_nonexistant():
+    # store a file
+    fileName = (''.join(random.choice(string.ascii_lowercase) for i in range(4)))
+    storeInvoice(xml_as_string,fileName)
+
+    # double check file has been stored
+    output = extract(fileName)
+    assert output[0] == fileName
+
+    # try and delete a different file
+    # the random file name generated is 4 characters
+    fileName = 'abc'
+    output = removeInvoice(fileName)
+    assert output == "File name does not exist"
