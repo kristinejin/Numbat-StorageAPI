4082ebf33862d2097d646b7e1f72e8021afe3161
Matthew Druckman <matthew.druckman@gmail.com>
2022-03-14 00:30:46 +1100

    created initial server testing for extract and remove

---
+++ test/test_extract_flask.py
@@ -0,0 +1,37 @@
+# import pytest
+# from src.flaskServer import app
+
+
+# @pytest.fixture
+# def client():
+#     return app.test_client()
+
+
+# def test_extract_basic(client):
+#     # Store 'xml'
+#     resp = client.get("/store")
+#     assert resp.status_code == 200
+#     resp = client.post("/store", data={"fnm": "Test", "xmll": "Pls"})
+#     assert resp.status_code == 200
+
+#     # Extract stored 'xml'
+#     resp = client.get("/extract")
+#     print(resp.data)
+#     assert resp.status_code == 200
+#     resp = client.post("/extract", data={"efn": "Test"})
+#     assert resp.status_code == 200
+
+
+# def test_extract_not_there(client):
+#     # Store 'xml'
+#     resp = client.get("/store")
+#     assert resp.status_code == 200
+#     resp = client.post("/store", data={"fnm": "Test", "xmll": "Pls"})
+#     assert resp.status_code == 200
+
+#     # Extract stored 'xml'
+#     resp = client.get("/extract")
+#     print(resp.data)
+#     assert resp.status_code == 200
+#     resp = client.post("/extract", data={"efn": "No-name"})
+#     assert resp.status_code == 400
