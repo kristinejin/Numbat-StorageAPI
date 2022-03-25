fb47707e4e33cefb5d317aa943914ef1690b44ec
Matthew Druckman <matthew.druckman@gmail.com>
2022-03-25 22:55:47 +1100

    debugged testing files for extract function

--- test/test_extract_flask.py
+++ test/test_extract_flask.py
@@ -2,14 +2,12 @@ import pytest
 import random
 import string
 from src.flask_server import app
+import requests
+from test.xml_str import xml_as_string
 
+URL = "http://127.0.0.1:5000"
 
-@pytest.fixture
-def client():
-    return app.test_client()
-
-
-def test_extract_basic(client):
+def test_extract_basic():
     file_name = (''.join(random.choice(string.ascii_lowercase)
                  for i in range(10)))
     password = (''.join(random.choice(string.ascii_lowercase)
@@ -17,18 +15,20 @@ def test_extract_basic(client):
     # Store 'xml'
     # resp = client.get("/store")
     # assert resp.status_code == 200
-    resp = client.post("/store", data={"FileName": file_name, "XML": "Pls", "Password": password})
+    resp = requests.post(URL + "/store", json={"FileName": file_name, "XML": xml_as_string, "Password": password})
     assert resp.status_code == 200
 
     # Extract stored 'xml'
     # resp = client.get("/extract")
     # print(resp.data)
-    assert resp.status_code == 200
-    resp = client.post("/extract", data={"FileName": file_name, "Password": password})
+    # assert resp.status_code == 200
+    resp = requests.post(URL + "/extract", json={"FileName": file_name, "Password": password})
+    assert resp.json()["XML"][0] == xml_as_string
+    # assert resp.content() == "pls"
     assert resp.status_code == 200
 
 
-def test_extract_not_there(client):
+def test_extract_not_there():
     file_name = (''.join(random.choice(string.ascii_lowercase)
                  for i in range(10)))
     password = (''.join(random.choice(string.ascii_lowercase)
@@ -36,14 +36,14 @@ def test_extract_not_there(client):
     
     # resp = client.get("/store")
     # assert resp.status_code == 200
-    resp = client.post("/store", data={"FileName": file_name, "XML": "Pls", "Password": password})
+    resp = requests.post(URL + "/store", json={"FileName": file_name, "XML": xml_as_string, "Password": password})
     assert resp.status_code == 200
 
     # Extract stored 'xml'
     # resp = client.get("/extract")
     # print(resp.data)
-    assert resp.status_code == 200
-    resp = client.post("/extract", data={"FileName": file_name + 'i', "Password": password + 'i'})
-    assert resp.status_code == 200
+    # assert resp.status_code == 200
+    resp = requests.post(URL + "/extract", json={"FileName": file_name + 'i', "Password": password + 'i'})
+    assert resp.status_code == 400
 
 
